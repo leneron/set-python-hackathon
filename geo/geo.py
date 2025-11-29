ae7934eac401c77d8fb1d34d5174924c85b12d95
Tetiana Ohnieva <tetiana.ohnieva@gmail.com>
2025-11-29 12:07:53 +0200

    Read all csvs

--- geo/geo.py
+++ geo/geo.py
@@ -1,6 +1,14 @@
 import geopandas
-import geodatasets
 
-gdf = geopandas.read_file('~/dev/set-python-hackathon/data/MTA_Subway_Stations_20251129.csv')
+DATA_DIR = "../data/"
 
-print(gdf)
+subway = geopandas.read_file(f'{DATA_DIR}/MTA_Subway_Stations_20251129.csv')
+parks = geopandas.read_file(f'{DATA_DIR}/Parks_Properties_20251129.csv')
+restaurants = geopandas.read_file(f'{DATA_DIR}/nyc_restaurant_dohmh_20251129.csv')
+restaurants_insp = geopandas.read_file(f'{DATA_DIR}/DOHMH_New_York_City_Restaurant_Inspection_Results_20251129.csv')
+
+
+print(subway)
+print(parks)
+print(restaurants)
+print(restaurants_insp)
