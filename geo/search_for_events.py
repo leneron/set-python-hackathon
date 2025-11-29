64d8c635cf8c9c74a35f2d4d335943bd827c4721
dekogen <denkondrat13@gmail.com>
2025-11-29 15:02:01 +0200

    Final search implementation (I hope)

--- geo/search_for_events.py
+++ geo/search_for_events.py
@@ -1,44 +1,42 @@
 import geopandas as gpd
-import pandas as pd
 from shapely.geometry import Point
 import numpy as np
 from sklearn.neighbors import BallTree
 from geo import prepare_data
 
 
-def calculate_distances(candidates, reference_gdf, radius=1000):
-    """
-    Обчислює відстань до найближчих точок із reference_gdf для кожної точки кандидатів.
-    """
-    # Конвертація в радіани (BallTree вимагає радіан для точок координат)
+def calculate_distances_and_names(candidates, reference_gdf, reference_name_column="Name"):
+    """Find the closest location (distance, name) in the reference_gdf to the candidates."""
     candidates_radians = np.radians(np.array(list(candidates.geometry.apply(lambda p: [p.x, p.y]))))
     reference_radians = np.radians(np.array(list(reference_gdf.geometry.apply(lambda p: [p.x, p.y]))))
 
-    # Побудова дерева для запитів
     tree = BallTree(reference_radians, metric='haversine')
 
-    # Шукаємо найближчі точки
-    distances, _ = tree.query(candidates_radians, k=1)
-    # Конвертуємо відстані з радіан у метри (радіус Землі ~6371 км)
-    distances_in_meters = distances * 6371000  # Земля в метрах
-    return distances_in_meters
+    # Find the closest dist & name
+    distances, indices = tree.query(candidates_radians, k=1)
+    distances_in_meters = distances * 6371000  # Earth radius, I guess;)
+    nearest_names = reference_gdf.iloc[indices.flatten()][reference_name_column].values
 
+    return distances_in_meters, nearest_names
 
-def get_nicest_locations(grid_step=0.01, metro_weight=2.0, rest_weight=1.0, park_weight=3.0):
-    nicerest, parks, metro = prepare_data()
-
+def create_candidates_to_score(grid_step):
+    """Create a grid of points anywhere in NY to score."""
     ny_bbox = (-74.259090, 40.477399, -73.700181, 40.917576)  # Межі Нью-Йорка
     lon_values = np.arange(ny_bbox[0], ny_bbox[2], grid_step)
     lat_values = np.arange(ny_bbox[1], ny_bbox[3], grid_step)
     candidate_points = [Point(lon, lat) for lon in lon_values for lat in lat_values]
-    candidates_gdf = gpd.GeoDataFrame({'geometry': candidate_points}, geometry='geometry')
+    return gpd.GeoDataFrame({'geometry': candidate_points}, geometry='geometry')
 
-    candidates_gdf['dist_to_metro'] = calculate_distances(candidates_gdf, metro)
-    candidates_gdf['dist_to_rest'] = calculate_distances(candidates_gdf, nicerest)
-    candidates_gdf['dist_to_park'] = calculate_distances(candidates_gdf, parks)
 
-    # Розрахунок рейтингу
-    # Чим ближче до об'єктів, тим вищий рейтинг
+def get_nicest_locations(grid_step=0.01, metro_weight=2.0, rest_weight=1.0, park_weight=3.0):
+    """Find best places in NY according to weitghts."""
+    nicerest, parks, metro = prepare_data()
+    candidates_gdf = create_candidates_to_score(grid_step=grid_step)
+
+    candidates_gdf['dist_to_metro'], candidates_gdf['metro_name']  = calculate_distances_and_names(candidates_gdf, metro)
+    candidates_gdf['dist_to_rest'], candidates_gdf["rest_name"] = calculate_distances_and_names(candidates_gdf, nicerest)
+    candidates_gdf['dist_to_park'], candidates_gdf["park_name"] = calculate_distances_and_names(candidates_gdf, parks)
+
     candidates_gdf['score'] = (
             - candidates_gdf['dist_to_metro'] * metro_weight
             - candidates_gdf['dist_to_rest'] * rest_weight
